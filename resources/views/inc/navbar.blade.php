06efbaf6d4019f8e9f8f3543f5cbc0897eaf3f68
Ridlafalah <fadelridlafalah@gmail.com>
2020-02-11 16:46:11 +0700

    update asset icon nav

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,24 +3,24 @@
     <div class="page-sidebar-inner">
         <ul class="menu accordion-menu">
             <li class="nav-heading"><span>Navigation</span></li>
-            
+
             {{-- Consumed dan Follop Up hanya Agent --}}
             @if (Auth::User()->divisi == 'Agent')
             <li><a href="/agent/workspace"><span class="menu-icon icon-speedometer"></span><p>Workspace</p></a></li>
-            <li><a href="/agent/consume/all"><span class="menu-icon icon-user"></span><p>Consumed</p></a></li>
+            <li><a href="/agent/consume/all"><span class="menu-icon icon-list"></span><p>Consumed</p></a></li>
             @endif
 
             {{-- Consumed dan Return hanya QCO --}}
             @if (Auth::User()->divisi == 'QCO')
             <li><a href="/qco/workspace"><span class="menu-icon icon-speedometer"></span><p>Workspace</p></a></li>
-            <li><a href="/qco/consume/all"><span class="menu-icon icon-user"></span><p>Consumed</p></a></li>
+            <li><a href="/qco/consume/all"><span class="menu-icon icon-list"></span><p>Consumed</p></a></li>
             @endif
 
             {{-- Navbar Admin --}}
             @if (Auth::User()->divisi == 'Admin')
             <li><a href="/admin"><span class="menu-icon icon-speedometer"></span><p>Dashboard</p></a></li>
-            <li><a href="/admin/report"><span class="menu-icon icon-user"></span><p>Report</p></a></li>
-            <li><a href="#"><span class="menu-icon icon-user"></span><p>Console</p></a>
+            <li><a href="/admin/report"><span class="menu-icon icon-bar-chart"></span><p>Report</p></a></li>
+            <li><a href="#"><span class="menu-icon icon-settings"></span><p>Console</p></a>
                 <ul class="sub-menu">
                     <li><a href="/admin/console/users">User</a></li>
                     <li><a href="/admin/console/resources">Resource</a></li>
@@ -36,7 +36,7 @@
                     <li><a href="message-view.html">View Message</a></li>
                     <li><a href="compose.html">Compose</a></li>
                 </ul>
-            </li> 
+            </li>
 
             <li class="nav-heading"><span>Features</span></li>
             <li class="droplink"><a href="#"><span class="menu-icon icon-briefcase"></span><p>UI Kits</p><span class="arrow"></span></a>
@@ -65,5 +65,5 @@
     @if ($datas['dev_message'])
         @include('inc.message')
     @endif
-    
+
 @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,146 +1,149 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- CSRF Token --}}
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+  {{-- CSRF Token --}}
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+  <title>{{ config('app.name') }}</title>
 
-        {{-- Fonts --}}
-        <link href="{{ asset('css/css-nunito.css') }}" rel="stylesheet">
-        <link href='{{ asset('css/css-family-ubuntu.css') }}' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 
-        {{-- Styles --}}
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/pace-master/themes/blue/pace-theme-flash.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/uniform/css/uniform.default.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/fontawesome/css/font-awesome.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/line-icons/simple-line-icons.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/waves/waves.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/switchery/switchery.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/3d-bold-navigation/css/style.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/slidepushmenus/css/component.css') }}" rel="stylesheet">
-        <link href="{{ asset('plugins/toastr/toastr.min.css')}}" rel="stylesheet"/>
-        {{-- Application Styles --}}
-        <link href="{{ asset('plugins/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css"/>
-        <link href="{{ asset('plugins/bootstrap-datepicker/css/datepicker3.css') }}" rel="stylesheet" type="text/css"/>
-        <link href="{{ asset('plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css"/>
-        {{-- Datatables --}}
-        <link href="{{ asset('plugins/datatables/css/jquery.datatables.min.css') }}" rel="stylesheet" type="text/css"/> 
-        <link href="{{ asset('plugins/datatables/css/jquery.datatables_themeroller.css') }}" rel="stylesheet" type="text/css"/> 
+  {{-- Fonts --}}
+  <link href="{{ asset('css/css-nunito.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/css-family-ubuntu.css') }}" rel='stylesheet' type='text/css'>
 
-        {{-- DatePicker --}}
-        <link href="{{ asset('plugins/bootstrap-datepicker/css/datepicker3.css')}}" rel="stylesheet" type="text/css"/>
-        
+  {{-- Styles --}}
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/pace-master/themes/blue/pace-theme-flash.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/uniform/css/uniform.default.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/fontawesome/css/font-awesome.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/line-icons/simple-line-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/waves/waves.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/switchery/switchery.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/3d-bold-navigation/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/slidepushmenus/css/component.css') }}" rel="stylesheet">
+  <link href="{{ asset('plugins/toastr/toastr.min.css')}}" rel="stylesheet" />
+  {{-- Application Styles --}}
+  <link href="{{ asset('plugins/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('plugins/bootstrap-datepicker/css/datepicker3.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css" />
+  {{-- Datatables --}}
+  <link href="{{ asset('plugins/datatables/css/jquery.datatables.min.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('plugins/datatables/css/jquery.datatables_themeroller.css') }}" rel="stylesheet" type="text/css" />
 
-        {{-- Theme Styles --}}
-        <link href="{{ asset('css/modern.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+  {{-- DatePicker --}}
+  <link href="{{ asset('plugins/bootstrap-datepicker/css/datepicker3.css')}}" rel="stylesheet" type="text/css" />
 
-        {{-- Scripts --}}
-        <script src="{{ asset('plugins/jquery/jquery-2.1.4.min.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/jquery-blockui/jquery.blockui.js') }}" defer></script>
-        <script src="{{ asset('plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/uniform/jquery.uniform.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/3d-bold-navigation/js/modernizr.js') }}" defer></script>
-        <script src="{{ asset('plugins/pace-master/pace.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/switchery/switchery.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/classie/classie.js') }}" defer></script>
-        <script src="{{ asset('plugins/waves/waves.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/3d-bold-navigation/js/main.js') }}" defer></script>
-        <script src="{{ asset('js/modern.min.js') }}" defer></script>
-        <script src="{{ asset('plugins/toastr/toastr.min.js')}}" defer></script>
 
-    </head>
-    <body class="page-header-fixed compact-menu page-horizontal-bar">
-        <div class="overlay"></div>
-        <main class="page-content content-wrap">
-            @auth
-                <div class="navbar">
-                    <div class="navbar-inner container">
-                        <div class="sidebar-pusher">
-                            <a href="javascript:void(0);" class="waves-effect waves-button waves-classic push-sidebar">
-                                <i class="fa fa-bars"></i>
-                            </a>
-                        </div>
-                        <div class="logo-box">
-                            {{-- <a href="/" class="logo-text"><span>{{ config('app.name') }}</span></a> --}}
-                            <a href="/" class="logo-image"><img class="logo-image" src="{{ asset('images/logo.png') }}" width="100" height="50" alt="Syifa"></a>
-                        </div>{{-- Logo Box --}}
-                        <div class="topmenu-outer">
-                            <div class="top-menu">
-                                <ul class="nav navbar-nav navbar-right">
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle waves-effect waves-button waves-classic" data-toggle="dropdown">
-                                            <span class="user-name">{{ Auth::user()->name }}<i class="fa fa-angle-down"></i></span>
-                                            <img class="img-circle avatar" src="{{ asset('images/avatar1.png') }}" width="40" height="40" alt="">
-                                        </a>
-                                        <ul class="dropdown-menu dropdown-list" role="menu">
-                                            <li role="presentation"><a href="{{ route('logout') }}" onclick="event.preventDefault();
+  {{-- Theme Styles --}}
+  <link href="{{ asset('css/modern.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+  {{-- Scripts --}}
+  <script src="{{ asset('plugins/jquery/jquery-2.1.4.min.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/jquery-blockui/jquery.blockui.js') }}" defer></script>
+  <script src="{{ asset('plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/uniform/jquery.uniform.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/3d-bold-navigation/js/modernizr.js') }}" defer></script>
+  <script src="{{ asset('plugins/pace-master/pace.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/switchery/switchery.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/classie/classie.js') }}" defer></script>
+  <script src="{{ asset('plugins/waves/waves.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/3d-bold-navigation/js/main.js') }}" defer></script>
+  <script src="{{ asset('js/modern.min.js') }}" defer></script>
+  <script src="{{ asset('plugins/toastr/toastr.min.js')}}" defer></script>
+
+</head>
+
+<body class="page-header-fixed compact-menu page-horizontal-bar">
+  <div class="overlay"></div>
+  <main class="page-content content-wrap">
+    @auth
+    <div class="navbar">
+      <div class="navbar-inner container">
+        <div class="sidebar-pusher">
+          <a href="javascript:void(0);" class="waves-effect waves-button waves-classic push-sidebar">
+            <i class="fa fa-bars"></i>
+          </a>
+        </div>
+        <div class="logo-box">
+          {{-- <a href="/" class="logo-text"><span>{{ config('app.name') }}</span></a> --}}
+          <a href="/" class="logo-image"><img class="logo-image" src="{{ asset('images/logo.png') }}" width="100" height="50" alt="Syifa"></a>
+        </div>{{-- Logo Box --}}
+        <div class="topmenu-outer">
+          <div class="top-menu">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle waves-effect waves-button waves-classic" data-toggle="dropdown">
+                  <span class="user-name">{{ Auth::user()->name }}<i class="fa fa-angle-down"></i></span>
+                  <img class="img-circle avatar" src="{{ asset('images/avatar1.png') }}" width="40" height="40" alt="">
+                </a>
+                <ul class="dropdown-menu dropdown-list" role="menu">
+                  <li role="presentation"><a href="{{ route('logout') }}" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();"><i class="fa fa-sign-out m-r-xs"></i>{{ __('Logout') }}</a></li>
-                                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                @csrf
-                                            </form>
-                                        </ul>
-                                    </li>
-                                </ul>{{-- Nav --}}
-                            </div>{{-- Top Menu --}}
-                        </div>
-                    </div>
-                </div>
-            @endauth
-            @auth
-                {{-- Navbar Start --}}
-                @include('inc.navbar')
-                {{-- Navbar End --}}
-            @endauth
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                  </form>
+                </ul>
+              </li>
+            </ul>{{-- Nav --}}
+          </div>{{-- Top Menu --}}
+        </div>
+      </div>
+    </div>
+    @endauth
+    @auth
+    {{-- Navbar Start --}}
+    @include('inc.navbar')
+    {{-- Navbar End --}}
+    @endauth
 
-            {{--  Page Sidebar --}}
-            <div class="page-inner">
-                {{-- Main Wrapper Start --}}
-                <div id="main-wrapper" class="container">
-                    @yield('content')
-                </div>
-                {{-- Main Wrapper End --}}
-                <div class="page-footer">
-                    <div class="container">
-                        <p class="no-s">2019 &copy; Infomedia.</p>
-                    </div>
-                </div>
-            </div>{{-- Page Inner End --}}
-        </main>{{-- Page Content End --}}
-        <div class="cd-overlay"></div>
-        <script type="text/javascript" defer>
-            // Func Notification
-            function notificationScript(type, title, message) {
-                console.log('Notification will start')
-                toastr.options = {
-                  "closeButton": false,
-                  "debug": false,
-                  "newestOnTop": false,
-                  "progressBar": false,
-                  "positionClass": "toast-top-center",
-                  "preventDuplicates": false,
-                  "onclick": null,
-                  "showDuration": "300",
-                  "hideDuration": "1000",
-                  "timeOut": "5000",
-                  "extendedTimeOut": "1000",
-                  "showEasing": "swing",
-                  "hideEasing": "linear",
-                  "showMethod": "fadeIn",
-                  "hideMethod": "fadeOut"
-                }
-                toastr[type](title, message)
-            }
-        </script>
+    {{-- Page Sidebar --}}
+    <div class="page-inner">
+      {{-- Main Wrapper Start --}}
+      <div id="main-wrapper" class="container">
+        @yield('content')
+      </div>
+      {{-- Main Wrapper End --}}
+      <div class="page-footer">
+        <div class="container">
+          <p class="no-s">2019 &copy; Infomedia.</p>
+        </div>
+      </div>
+    </div>{{-- Page Inner End --}}
+  </main>{{-- Page Content End --}}
+  <div class="cd-overlay"></div>
+  <script type="text/javascript" defer>
+    // Func Notification
+    function notificationScript(type, title, message) {
+      console.log('Notification will start')
+      toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      }
+      toastr[type](title, message)
+    }
+  </script>
+
 </html>
